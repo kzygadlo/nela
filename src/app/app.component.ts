a3baf53fc38e7eb12d0c1bbdc6fa70ffc2cd919d
kzygadlo <krzysiek.zygadlo@gmail.com>
2024-06-27 22:11:30 +0200

    main page v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,26 +5,45 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
-
 import { MatDividerModule } from '@angular/material/divider';
+import { differenceInMonths, differenceInDays, addMonths } from 'date-fns';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DatePipe, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatDividerModule],
+  imports: [
+    RouterOutlet, 
+    DatePipe, 
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatMenuModule, 
+    MatDividerModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
+
   constructor(private router: Router) {}
   name: string = 'Nela';
   dateOfBirth: Date = new Date("2024-03-11");  
   pesel: string = '24231103844';
   medicover: string = '4646439';
+  age = calculateAge(this.dateOfBirth);
 
   goToHome() {
     this.router.navigate(['/']); // Zakładając, że główna strona jest na '/' ścieżce
   }
 
+}
+
+function calculateAge(birthDate: Date): { months: number, days: number } {
+  const today = new Date();
+  const months = differenceInMonths(today, birthDate);
+  const dateAfterMonths = addMonths(birthDate, months);
+  const days = differenceInDays(today, dateAfterMonths);
+
+  return { months, days };
 }
