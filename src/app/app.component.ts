68a770897a69a63b6d62ed0fd297767682dc58df
kzygadlo <krzysiek.zygadlo@gmail.com>
2024-06-27 16:10:49 +0200

    main page v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
 
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DatePipe, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, DatePipe, MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
+  constructor(private router: Router) {}
   name: string = 'Nela';
   dateOfBirth: Date = new Date("2024-03-11");  
+  pesel: string = '24231103844';
+  medicover: string = '4646439';
+
+  goToHome() {
+    this.router.navigate(['/']); // Zakładając, że główna strona jest na '/' ścieżce
+  }
+
 }
